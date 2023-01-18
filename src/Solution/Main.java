bd0127f252ddecf408627d909d520f9107593791
EoJinChoi <01031763910a@gmail.com>
2023-01-18 15:16:49 +0900

    complete

---
+++ src/Solution/Main.java
@@ -0,0 +1,12 @@
+package Solution;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int[] arr = {2,2,1,1,1,2,2};
+		
+		int m = Solution.majorityElement(arr);
+		System.out.println(m);
+	}
+
+}
