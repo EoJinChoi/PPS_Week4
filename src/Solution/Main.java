d363e2f08df50eb0bde381ef92d5197d6508b3a1
EoJinChoi <01031763910a@gmail.com>
2023-01-18 14:59:27 +0900

    complete

---
+++ src/Solution/Main.java
@@ -0,0 +1,12 @@
+package Solution;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int[] ar = {1, 5, 2, 6, 3, 7, 4};
+		int[][] co = {{2, 5, 3}, {4, 4, 1}, {1, 7, 3}};
+		
+		Solution.solution(ar, co);
+	}
+
+}
