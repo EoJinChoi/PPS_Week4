68e61ebbc37a7288b59a3131fea526ef08825315
EoJinChoi <01031763910a@gmail.com>
2023-01-19 17:16:48 +0900

    complete

---
+++ src/Solution/Main.java
@@ -0,0 +1,11 @@
+package Solution;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int[][] a = {{5,10}, {2,5}, {4,7},{3,9}};
+		int size = 10;
+		
+		Solution.maximumUnits(a, size);
+	}
+}
