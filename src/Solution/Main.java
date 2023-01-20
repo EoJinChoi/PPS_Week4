1d7552bead0010c97e90773c975d1164d126f230
EoJinChoi <01031763910a@gmail.com>
2023-01-20 23:51:54 +0900

    complete

---
+++ src/Solution/Main.java
@@ -0,0 +1,12 @@
+package Solution;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int[] n1 = {1,2,3,0,0,0};
+		int[] n2 = {2,5,6};
+		
+		Solution.merge(n1, 3, n2, 3);
+	}
+
+}
